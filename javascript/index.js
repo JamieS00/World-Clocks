897dd6a8299e7c585a878b941b34489cdf97560f
Jamie Singleton <43870204+JamieS00@users.noreply.github.com>
2024-10-31 19:22:27 -0400

    added js code for time & date

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,33 @@
+//LOS ANGELES
+let updateTime = () => {
+  let laElement = document.querySelector("#los-angeles");
 
+  //date & time element : target the classes date and time withing the LA div
+  let laDateElement = laElement.querySelector(".date");
+  let laTimeElement = laElement.querySelector(".time");
+
+  let LAdateFormat = moment.tz("America/Los_Angeles").format("MMMM Do YYYY");
+  laDateElement.innerHTML = LAdateFormat;
+
+  let LAtimeFormat = moment
+    .tz("America/Los_Angeles")
+    .format("h:mm:ss [<small>]A[</small>]");
+  laTimeElement.innerHTML = LAtimeFormat;
+
+  //SYDNEY
+  let sydneyElement = document.querySelector("#sydney");
+
+  //date & time element : target the class date and time in the sydney div
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+
+  let sydneydateFormat = moment.tz("Australia/Sydney").format("MMMM Do YYYY");
+  sydneyDateElement.innerHTML = sydneydateFormat;
+
+  let sydneytimeFormat = moment
+    .tz("Australia/Sydney")
+    .format("h:mm:ss [<small>]A[</small>]");
+  sydneyTimeElement.innerHTML = sydneytimeFormat;
+};
+
+setInterval(updateTime, 1000);
